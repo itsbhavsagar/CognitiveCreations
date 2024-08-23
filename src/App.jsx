fc6b1a25daaac2faf66fc15a34a3dee394558057
Bhavsagar <bhavsagar@protonmail.com>
2024-08-23 18:02:25 +0530

    added services page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import Services from "./components/Services";
 export default function App() {
   return (
     <>
       <Navbar />
       <Header />
+      <Services />
     </>
   );
 }
---
+++ src/components/Services.jsx
@@ -0,0 +1,200 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <>
+      <section class="mx-auto px-6 -mt-20 text-gray-800 md:max-w-screen-xl">
+        <div class="mx-auto mt-20 mb-16 block px-6 text-center">
+          <h2 class="mx-auto  font-bold text-2xl lg:text-5xl ">
+            Your
+            <span className="md:text-4xl text-2xl lg:text-5xl mx-auto bg-sky-400a mr-6 ml-6 inline-flex h-16 items-center justify-center rounded-full bg-gradient-to-r from-lime-400 to-sky-400 px-8 font-medium tracking-wide text-white shadow-lg shadow-sky-300 outline-none transition duration-200 hover:scale-110 hover:bg-sky-500 focus:ring">
+              {" "}
+              Digital Future{" "}
+            </span>
+            is Here
+          </h2>
+          <div class="mx-auto mt-6 mb-auto block w-full text-xl font-normal leading-9 text-gray-700 md:w-3/4 xl:w-3/4">
+            <p class="text-lg">
+              At Cognitive Creations, we bridge the gap between vision and
+              reality. Our expert team harnesses the power of cutting-edge
+              technology, AI, and creative strategies to build digital solutions
+              that propel your brand into the future. Whether it's innovative
+              apps, dynamic websites, or impactful marketing campaigns, we're
+              here to transform your ideas into tangible success.
+            </p>
+          </div>
+          <div class="mt-8 text-center">
+            <a
+              class="mx-2 mb-2 inline-block rounded bg-blue-500 px-6 py-2 text-xl font-medium text-white shadow md:mx-4 md:mt-2 md:text-lg hover:scale-105 hover:shadow-md"
+              href="#"
+              target="_blank"
+            >
+              Get Started
+            </a>
+            <a
+              class="mx-2 mb-2 inline-block rounded bg-lime-300  px-6 py-2 text-xl font-medium text-black shadow md:mx-4 md:mt-2 md:text-lg hover:scale-105 hover:shadow-md"
+              href="#"
+            >
+              Book a Demo
+            </a>
+          </div>
+        </div>
+
+        <div class="grid gap-10 pb-20 sm:grid-cols-2 lg:grid-cols-3">
+          <div class="rounded-md border border-gray-200 bg-white p-8 shadow-sm">
+            <div class="my-4 flex items-center">
+              <div class="mr-4 flex h-12 w-12 items-center justify-center rounded-lg border bg-blue-50 text-2xl text-blue-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  role="img"
+                  width="0.85em"
+                  height="1em"
+                  preserveAspectRatio="xMidYMid meet"
+                  viewBox="0 0 27 32"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M17.5 1a.5.5 0 0 0 0-1h-9a.5.5 0 0 0 0 1h9zM.99 27.635c-1.074 1.511-.954 2.498-.664 3.06C.633 31.29 1.433 32 3.5 32h20c2.067 0 2.867-.71 3.174-1.306c.29-.562.41-1.549-.648-3.034l-6.219-9.95l-.088-.124C19.272 16.948 17 13.022 17 9.75V2.5a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0-.5.5v7.25c0 3.491-2.465 7.957-2.493 8.005L.99 27.635zm24.796 2.6c-.251.487-1.084.765-2.286.765h-20c-1.202 0-2.035-.278-2.286-.765c-.229-.444-.02-1.162.62-2.066l4.999-8.948l.007.004c.91.525 1.851 1.068 3.719 1.068s2.81-.542 3.719-1.066c.833-.48 1.619-.934 3.22-.934c.607 0 1.133.075 1.617.21l6.08 9.712c.611.858.82 1.576.591 2.02zM10 9.75V3h6v6.75c0 2.84 1.516 6.042 2.404 7.602a7.862 7.862 0 0 0-.905-.059c-1.869 0-2.81.542-3.719 1.066c-.833.48-1.619.934-3.22.934c-1.601 0-2.387-.454-3.219-.934l-.019-.011l.046-.082C7.393 18.226 10 13.58 10 9.75z"
+                  />
+                </svg>
+              </div>
+              <h3 class="text-2xl font-bold md:text-xl">Enhanced</h3>
+            </div>
+            <p class="text-gray-700">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+              repellendus.
+            </p>
+          </div>
+          <div class="rounded-md border border-gray-200 bg-white p-8 shadow-sm">
+            <div class="my-4 flex items-center">
+              <div class="mr-4 flex h-12 w-12 items-center justify-center rounded-lg border bg-blue-50 text-2xl text-blue-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  role="img"
+                  width="0.85em"
+                  height="1em"
+                  preserveAspectRatio="xMidYMid meet"
+                  viewBox="0 0 27 32"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M17.5 1a.5.5 0 0 0 0-1h-9a.5.5 0 0 0 0 1h9zM.99 27.635c-1.074 1.511-.954 2.498-.664 3.06C.633 31.29 1.433 32 3.5 32h20c2.067 0 2.867-.71 3.174-1.306c.29-.562.41-1.549-.648-3.034l-6.219-9.95l-.088-.124C19.272 16.948 17 13.022 17 9.75V2.5a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0-.5.5v7.25c0 3.491-2.465 7.957-2.493 8.005L.99 27.635zm24.796 2.6c-.251.487-1.084.765-2.286.765h-20c-1.202 0-2.035-.278-2.286-.765c-.229-.444-.02-1.162.62-2.066l4.999-8.948l.007.004c.91.525 1.851 1.068 3.719 1.068s2.81-.542 3.719-1.066c.833-.48 1.619-.934 3.22-.934c.607 0 1.133.075 1.617.21l6.08 9.712c.611.858.82 1.576.591 2.02zM10 9.75V3h6v6.75c0 2.84 1.516 6.042 2.404 7.602a7.862 7.862 0 0 0-.905-.059c-1.869 0-2.81.542-3.719 1.066c-.833.48-1.619.934-3.22.934c-1.601 0-2.387-.454-3.219-.934l-.019-.011l.046-.082C7.393 18.226 10 13.58 10 9.75z"
+                  />
+                </svg>
+              </div>
+              <h3 class="text-2xl font-bold md:text-xl">Performant</h3>
+            </div>
+            <p class="text-gray-700">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+              repellendus.
+            </p>
+          </div>
+          <div class="rounded-md border border-gray-200 bg-white p-8 shadow-sm">
+            <div class="my-4 flex items-center">
+              <div class="mr-4 flex h-12 w-12 items-center justify-center rounded-lg border bg-blue-50 text-2xl text-blue-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  role="img"
+                  width="0.85em"
+                  height="1em"
+                  preserveAspectRatio="xMidYMid meet"
+                  viewBox="0 0 27 32"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M17.5 1a.5.5 0 0 0 0-1h-9a.5.5 0 0 0 0 1h9zM.99 27.635c-1.074 1.511-.954 2.498-.664 3.06C.633 31.29 1.433 32 3.5 32h20c2.067 0 2.867-.71 3.174-1.306c.29-.562.41-1.549-.648-3.034l-6.219-9.95l-.088-.124C19.272 16.948 17 13.022 17 9.75V2.5a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0-.5.5v7.25c0 3.491-2.465 7.957-2.493 8.005L.99 27.635zm24.796 2.6c-.251.487-1.084.765-2.286.765h-20c-1.202 0-2.035-.278-2.286-.765c-.229-.444-.02-1.162.62-2.066l4.999-8.948l.007.004c.91.525 1.851 1.068 3.719 1.068s2.81-.542 3.719-1.066c.833-.48 1.619-.934 3.22-.934c.607 0 1.133.075 1.617.21l6.08 9.712c.611.858.82 1.576.591 2.02zM10 9.75V3h6v6.75c0 2.84 1.516 6.042 2.404 7.602a7.862 7.862 0 0 0-.905-.059c-1.869 0-2.81.542-3.719 1.066c-.833.48-1.619.934-3.22.934c-1.601 0-2.387-.454-3.219-.934l-.019-.011l.046-.082C7.393 18.226 10 13.58 10 9.75z"
+                  />
+                </svg>
+              </div>
+              <h3 class="text-2xl font-bold md:text-xl">Analytical</h3>
+            </div>
+            <p class="text-gray-700">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+              repellendus.
+            </p>
+          </div>
+          <div class="rounded-md border border-gray-200 bg-white p-8 shadow-sm">
+            <div class="my-4 flex items-center">
+              <div class="mr-4 flex h-12 w-12 items-center justify-center rounded-lg border bg-blue-50 text-2xl text-blue-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  role="img"
+                  width="0.85em"
+                  height="1em"
+                  preserveAspectRatio="xMidYMid meet"
+                  viewBox="0 0 27 32"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M17.5 1a.5.5 0 0 0 0-1h-9a.5.5 0 0 0 0 1h9zM.99 27.635c-1.074 1.511-.954 2.498-.664 3.06C.633 31.29 1.433 32 3.5 32h20c2.067 0 2.867-.71 3.174-1.306c.29-.562.41-1.549-.648-3.034l-6.219-9.95l-.088-.124C19.272 16.948 17 13.022 17 9.75V2.5a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0-.5.5v7.25c0 3.491-2.465 7.957-2.493 8.005L.99 27.635zm24.796 2.6c-.251.487-1.084.765-2.286.765h-20c-1.202 0-2.035-.278-2.286-.765c-.229-.444-.02-1.162.62-2.066l4.999-8.948l.007.004c.91.525 1.851 1.068 3.719 1.068s2.81-.542 3.719-1.066c.833-.48 1.619-.934 3.22-.934c.607 0 1.133.075 1.617.21l6.08 9.712c.611.858.82 1.576.591 2.02zM10 9.75V3h6v6.75c0 2.84 1.516 6.042 2.404 7.602a7.862 7.862 0 0 0-.905-.059c-1.869 0-2.81.542-3.719 1.066c-.833.48-1.619.934-3.22.934c-1.601 0-2.387-.454-3.219-.934l-.019-.011l.046-.082C7.393 18.226 10 13.58 10 9.75z"
+                  />
+                </svg>
+              </div>
+              <h3 class="text-2xl font-bold md:text-xl">Enhanced</h3>
+            </div>
+            <p class="text-gray-700">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+              repellendus.
+            </p>
+          </div>
+          <div class="rounded-md border border-gray-200 bg-white p-8 shadow-sm">
+            <div class="my-4 flex items-center">
+              <div class="mr-4 flex h-12 w-12 items-center justify-center rounded-lg border bg-blue-50 text-2xl text-blue-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  role="img"
+                  width="0.85em"
+                  height="1em"
+                  preserveAspectRatio="xMidYMid meet"
+                  viewBox="0 0 27 32"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M17.5 1a.5.5 0 0 0 0-1h-9a.5.5 0 0 0 0 1h9zM.99 27.635c-1.074 1.511-.954 2.498-.664 3.06C.633 31.29 1.433 32 3.5 32h20c2.067 0 2.867-.71 3.174-1.306c.29-.562.41-1.549-.648-3.034l-6.219-9.95l-.088-.124C19.272 16.948 17 13.022 17 9.75V2.5a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0-.5.5v7.25c0 3.491-2.465 7.957-2.493 8.005L.99 27.635zm24.796 2.6c-.251.487-1.084.765-2.286.765h-20c-1.202 0-2.035-.278-2.286-.765c-.229-.444-.02-1.162.62-2.066l4.999-8.948l.007.004c.91.525 1.851 1.068 3.719 1.068s2.81-.542 3.719-1.066c.833-.48 1.619-.934 3.22-.934c.607 0 1.133.075 1.617.21l6.08 9.712c.611.858.82 1.576.591 2.02zM10 9.75V3h6v6.75c0 2.84 1.516 6.042 2.404 7.602a7.862 7.862 0 0 0-.905-.059c-1.869 0-2.81.542-3.719 1.066c-.833.48-1.619.934-3.22.934c-1.601 0-2.387-.454-3.219-.934l-.019-.011l.046-.082C7.393 18.226 10 13.58 10 9.75z"
+                  />
+                </svg>
+              </div>
+              <h3 class="text-2xl font-bold md:text-xl">Performant</h3>
+            </div>
+            <p class="text-gray-700">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+              repellendus.
+            </p>
+          </div>
+          <div class="rounded-md border border-gray-200 bg-white p-8 shadow-sm">
+            <div class="my-4 flex items-center">
+              <div class="mr-4 flex h-12 w-12 items-center justify-center rounded-lg border bg-blue-50 text-2xl text-blue-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  role="img"
+                  width="0.85em"
+                  height="1em"
+                  preserveAspectRatio="xMidYMid meet"
+                  viewBox="0 0 27 32"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M17.5 1a.5.5 0 0 0 0-1h-9a.5.5 0 0 0 0 1h9zM.99 27.635c-1.074 1.511-.954 2.498-.664 3.06C.633 31.29 1.433 32 3.5 32h20c2.067 0 2.867-.71 3.174-1.306c.29-.562.41-1.549-.648-3.034l-6.219-9.95l-.088-.124C19.272 16.948 17 13.022 17 9.75V2.5a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0-.5.5v7.25c0 3.491-2.465 7.957-2.493 8.005L.99 27.635zm24.796 2.6c-.251.487-1.084.765-2.286.765h-20c-1.202 0-2.035-.278-2.286-.765c-.229-.444-.02-1.162.62-2.066l4.999-8.948l.007.004c.91.525 1.851 1.068 3.719 1.068s2.81-.542 3.719-1.066c.833-.48 1.619-.934 3.22-.934c.607 0 1.133.075 1.617.21l6.08 9.712c.611.858.82 1.576.591 2.02zM10 9.75V3h6v6.75c0 2.84 1.516 6.042 2.404 7.602a7.862 7.862 0 0 0-.905-.059c-1.869 0-2.81.542-3.719 1.066c-.833.48-1.619.934-3.22.934c-1.601 0-2.387-.454-3.219-.934l-.019-.011l.046-.082C7.393 18.226 10 13.58 10 9.75z"
+                  />
+                </svg>
+              </div>
+              <h3 class="text-2xl font-bold md:text-xl">Analytical</h3>
+            </div>
+            <p class="text-gray-700">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+              repellendus.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Services;

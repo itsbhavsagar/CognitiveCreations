d7c31c2b25acbdddae60485049ad1302f67b4d23
Bhavsagar <bhavsagar@protonmail.com>
2024-08-23 15:09:51 +0530

    Added navbar and header

---
+++ src/components/Header.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,900&display=swap"
+        rel="stylesheet"
+      />
+      {/* <style>
+  * {
+  font-family: 'Source Sans Pro';
+  }
+</style> */}
+      <div class="flex min-h-screen w-screen font-sans">
+        <div class="relative my-auto mx-auto flex flex-col px-4 sm:max-w-xl md:max-w-screen-xl md:flex-row">
+          <div class="mx-auto flex w-full max-w-xl lg:max-w-screen-xl">
+            <div class="mb-16 lg:my-auto lg:max-w-lg">
+              <div class="mb-6 max-w-xl">
+                <div>
+                  <p class="bg-teal-accent-400 mb-2 inline-block rounded-full bg-lime-300 px-3 py-1 text-xs font-semibold uppercase tracking-wider text-indigo-900">
+                    Beyond Marketing Agency
+                  </p>
+                </div>
+                <h2 class="mb-6 max-w-lg text-3xl font-extrabold text-slate-700 sm:text-5xl sm:leading-snug">
+                  We drive growth to <br />
+                  your business
+                  <br />
+                  <span class="rounded- abg-gradient-to-r inline-block bg-sky-400 from-lime-400 to-sky-400 px-2 font-bold text-white">
+                    Beyond
+                  </span>
+                </h2>
+                <p class="text-base text-gray-700 md:text-lg">
+                  we specialize in transforming your vision into reality. Our
+                  expert team of marketers, designers, and strategists crafts
+                  bespoke solutions tailored to elevate your brand, drive
+                  engagement, and maximize growth. Discover the difference with
+                  our data-driven strategies and creative campaigns that capture
+                  attention and deliver results.
+                </p>
+              </div>
+              <div class="flex items-center">
+                <a
+                  href="/"
+                  class="bg-sky-400a mr-6 inline-flex h-12 items-center justify-center rounded-full bg-gradient-to-r from-lime-400 to-sky-400 px-8 font-medium tracking-wide text-white shadow-lg shadow-sky-300 outline-none transition duration-200 hover:scale-110 hover:bg-sky-500 focus:ring"
+                >
+                  {" "}
+                  Get started{" "}
+                </a>
+                <a
+                  href="/"
+                  class="inline-flex items-center font-semibold text-sky-400 transition-colors duration-200 hover:text-lime-400"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="mr-2 h-6 w-6"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  Watch Videos
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex h-full w-full space-x-3 overflow-hidden md:justify-end">
+            <div class="hidden w-56 items-center space-y-3 lg:flex">
+              <div class="overflow-hidden rounded-xl bg-yellow-400">
+                <img
+                  class="h-full w-full object-cover"
+                  src="https://www.wheelermarketingagency.com/wp-content/uploads/2023/10/Instagram-for-Business-819x1024.png"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div class="w-full flex-col space-y-3 rounded-xl py-4 lg:flex lg:w-80">
+              <div class="h-40 overflow-hidden rounded-xl bg-green-600/60">
+                <img
+                  class="mx-auto h-full w-full object-cover"
+                  src="https://cdn3d.iconscout.com/3d/premium/thumb/digital-marketing-3d-illustration-download-in-png-blend-fbx-gltf-file-formats--analytics-logo-business-growth-illustrations-2426540.png?f=webp"
+                  alt=""
+                />
+              </div>
+              <div class="h-40 overflow-hidden rounded-xl bg-pink-600/60">
+                <img
+                  class="mx-auto h-full w-full object-cover"
+                  src="https://img.freepik.com/free-psd/3d-rendering-recruitment-sales-background_23-2151323337.jpg?size=626&ext=jpg&ga=GA1.1.44546679.1716940800&semt=ais_user"
+                  alt=""
+                />
+              </div>
+              <div class="h-40 overflow-hidden rounded-xl bg-blue-600/60">
+                <img
+                  class="mx-auto h-full w-full object-cover"
+                  src="https://static.vecteezy.com/system/resources/previews/011/019/587/original/social-media-and-digital-marketing-3d-illustration-png.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header class="text-slate-700 container relative mx-auto flex flex-col overflow-hidden px-4 py-4 lg:flex-row lg:items-center shadow-md">
+      <a
+        href="#"
+        class="flex items-center whitespace-nowrap text-2xl font-black"
+      >
+        <span class="mr-2 text-4xl text-blue-600">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            role="img"
+            width="1em"
+            height="1em"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="M6.925 16.875Q5.2 16.225 4.1 14.713Q3 13.2 3 11.25q0-1.975.938-3.513Q4.875 6.2 6 5.15q1.125-1.05 2.062-1.6L9 3v2.475q0 .625.45 1.062q.45.438 1.075.438q.35 0 .65-.15q.3-.15.5-.425L12 6q.95.55 1.625 1.35t1.025 1.8l-1.675 1.675q-.05-.6-.287-1.175q-.238-.575-.638-1.05q-.35.2-.738.287q-.387.088-.787.088q-1.1 0-1.987-.612Q7.65 7.75 7.25 6.725q-.95.925-1.6 2.062Q5 9.925 5 11.25q0 .775.275 1.462q.275.688.75 1.213q.05-.5.287-.938q.238-.437.588-.787L9 10.1l2.15 2.1q.05.05.1.125t.1.125l-1.425 1.425q-.05-.075-.087-.125q-.038-.05-.088-.1L9 12.925l-.7.7q-.125.125-.212.287q-.088.163-.088.363q0 .3.175.537q.175.238.45.363ZM9 10.1Zm0 0ZM7.4 22L6 20.6L19.6 7L21 8.4L17.4 12H21v2h-5.6l-.5.5l1.5 1.5H21v2h-2.6l2.1 2.1l-1.4 1.4l-2.1-2.1V22h-2v-4.6l-1.5-1.5l-.5.5V22h-2v-3.6Z"
+            />
+          </svg>
+        </span>
+        Cognitive Creations
+      </a>
+      <input type="checkbox" class="peer hidden" id="navbar-open" />
+      <label
+        class="absolute top-5 right-5 cursor-pointer lg:hidden"
+        for="navbar-open"
+      >
+        <svg
+          class="h-7 w-7"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.5"
+            d="M4 6h16M4 12h16M4 18h16"
+          ></path>
+        </svg>
+      </label>
+      <nav
+        aria-label="Header Navigation"
+        class="peer-checked:pt-8 peer-checked:max-h-60 flex max-h-0 w-full flex-col items-center overflow-hidden transition-all lg:ml-24 lg:max-h-full lg:flex-row"
+      >
+        <ul class="flex w-full flex-col items-center space-y-2 lg:flex-row lg:justify-center lg:space-y-0">
+          <li class="lg:mr-12">
+            <a
+              class="rounded text-gray-700 transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2"
+              href="#"
+            >
+              Services
+            </a>
+          </li>
+          <li class="lg:mr-12">
+            <a
+              class="rounded text-gray-700 transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2"
+              href="#"
+            >
+              Work
+            </a>
+          </li>
+          <li class="lg:mr-12">
+            <a
+              class="rounded text-gray-700 transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2"
+              href="#"
+            >
+              Team
+            </a>
+          </li>
+          <li class="lg:mr-12">
+            <a
+              class="rounded text-gray-700 transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2"
+              href="#"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+        <hr class="mt-4 w-full lg:hidden" />
+        <div class="my-4 flex items-center space-x-6 space-y-2 lg:my-0 lg:ml-auto lg:space-x-8 lg:space-y-0">
+          <a
+            href="#"
+            title=""
+            class="whitespace-nowrap rounded-xl bg-lime-300  px-5 py-3 font-medium text-gray transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-offset-2 hover:bg-sky-400"
+          >
+            Book free consultation
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;

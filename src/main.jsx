1dc76057cbd03b6b0427d150d5e212fc6a687776
Bhavsagar <bhavsagar@protonmail.com>
2024-08-31 12:55:24 +0530

    renamed- layout folder to pages for better underst

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
-import Home from "./components/layout/Home.jsx";
+import Home from "./components/pages/Home.jsx";
 import Work from "./components/sections/Work.jsx";
 import Contact from "./components/sections/Contact.jsx";
 import "./index.css";
